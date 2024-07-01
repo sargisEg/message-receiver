1a29101c8a5ba8d1c16da522b90c2b34cdbd0a33
sargisyeghoyan <sargis.yeghoyan@ihost.am>
2024-07-01 16:32:13 +0400

    Added kafka consumer unit tests

--- src/main/java/org/receiver/messaging/consumer/KafkaConsumerImpl.java
+++ src/main/java/org/receiver/messaging/consumer/KafkaConsumerImpl.java
@@ -8,6 +8,7 @@ import org.receiver.service.core.CustomMessageService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
 
 @Component
 @RequiredArgsConstructor
@@ -19,12 +20,14 @@ public class KafkaConsumerImpl implements KafkaConsumer {
 
     @Override
     public void primaryMessage(CustomMessageDto dto) {
+        Assert.notNull(dto, "dto should not be null");
         LOGGER.debug("Received primary message - {}", dto);
         customMessageService.create(new CreateCustomMessageParams(dto.getTitle(), dto.getContent(), MessageType.PRIMARY));
     }
 
     @Override
     public void secondaryMessage(CustomMessageDto dto) {
+        Assert.notNull(dto, "dto should not be null");
         LOGGER.debug("Received secondary message - {}", dto);
         customMessageService.create(new CreateCustomMessageParams(dto.getTitle(), dto.getContent(), MessageType.SECONDARY));
     }
